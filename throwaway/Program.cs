d870a4b6a9e9e971228a375c1870b212547c6c25
17DaffoH <17DaffoH@fulford.york.sch.uk>
2022-09-09 14:44:20 +0100

    Add project files.

---
+++ throwaway/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace throwaway
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
