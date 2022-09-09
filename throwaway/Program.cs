c344f0169c2d4329ec33fc1b15d17c598c336d02
Heloise Dafforne <17DaffoH@fulford.york.sch.uk>
2022-09-09 15:03:53 +0100

    added code

--- throwaway/Program.cs
+++ throwaway/Program.cs
@@ -10,6 +10,14 @@ namespace throwaway
     {
         static void Main(string[] args)
         {
+            // Asks how many pizzas the user wants
+            Console.WriteLine("How many pizzas?");
+            int answer = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < answer; i++)  // Loops for the value of answer
+            {
+                Console.WriteLine("Eat pizza number " + (i+1));  // Counts up from one 
+            }    
         }
     }
 }
